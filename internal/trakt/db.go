8d328b5d05328af5f32b8b0c532f3e8ec523d3d7
Munif Tanjim <hello@muniftanjim.dev>
2025-08-23 19:33:25 +0600

    fix(trakt): db locked error for set id maps

--- internal/trakt/db.go
+++ internal/trakt/db.go
@@ -506,7 +506,7 @@ func upsertItems(tx db.Executor, items []TraktItem) error {
 				})
 			}
 		}
-		util.LogError(log, meta.SetIdMaps(idMaps, meta.IdProviderIMDB), "failed to set id maps")
+		util.LogError(log, meta.SetIdMapsInTrx(tx, idMaps, meta.IdProviderIMDB), "failed to set id maps")
 	}
 
 	return nil
