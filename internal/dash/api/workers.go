6bb941e947b635f05b4b26f0a5a31301c4a09d1d
Munif Tanjim <hello@muniftanjim.dev>
2025-11-19 00:42:48 +0600

    feat(dash/workers): add failure indicator in dropdown

--- internal/dash/api/workers.go
+++ internal/dash/api/workers.go
@@ -2,11 +2,19 @@ package dash_api
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/MunifTanjim/stremthru/internal/job_log"
 	"github.com/MunifTanjim/stremthru/internal/shared"
 	"github.com/MunifTanjim/stremthru/internal/worker"
 )
+
+type WorkerDetails struct {
+	Id           string        `json:"id"`
+	Title        string        `json:"title"`
+	Interval     time.Duration `json:"interval"`
+	HasFailedJob bool          `json:"has_failed_job"`
+}
 
 func HandleGetWorkersDetails(w http.ResponseWriter, r *http.Request) {
 	if !shared.IsMethod(r, http.MethodGet) {
@@ -14,7 +22,29 @@ func HandleGetWorkersDetails(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	SendData(w, r, 200, worker.WorkerDetailsById)
+	data := make(map[string]*WorkerDetails, len(worker.WorkerDetailsById))
+
+	for name, details := range worker.WorkerDetailsById {
+		data[name] = &WorkerDetails{
+			Id:       details.Id,
+			Title:    details.Title,
+			Interval: details.Interval,
+		}
+	}
+
+	failedWorkerNames, err := job_log.GetWorkerNamesWithFailedJobs()
+	if err != nil {
+		SendError(w, r, err)
+		return
+	}
+
+	for _, workerName := range failedWorkerNames {
+		if workerResp, ok := data[workerName]; ok {
+			workerResp.HasFailedJob = true
+		}
+	}
+
+	SendData(w, r, 200, data)
 }
 
 func HandleGetWorkerJobLogs(w http.ResponseWriter, r *http.Request) {
