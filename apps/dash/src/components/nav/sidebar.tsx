0f417f04fd51ce5a5289cc91e6780a9ff06eb530
Munif Tanjim <hello@muniftanjim.dev>
2025-12-22 23:35:28 +0600

    feat(dash/usenet): add nzb page

--- apps/dash/src/components/nav/sidebar.tsx
+++ apps/dash/src/components/nav/sidebar.tsx
@@ -10,6 +10,7 @@ import {
   LogOut,
   MagnetIcon,
   Moon,
+  NewspaperIcon,
   Sparkles,
   Sun,
   User,
@@ -280,6 +281,19 @@ function useNavItems(): NavItem[] {
     items.push(torrents);
 
     if (server?.feature.vault) {
+      const usenet: NavItem = {
+        icon: NewspaperIcon,
+        items: [
+          {
+            path: "/dash/usenet/nzb",
+            title: "NZB",
+          },
+        ],
+        path: "/dash/usenet",
+        title: "Usenet",
+      };
+      items.push(usenet);
+
       const vault: NavItem = {
         icon: Lock,
         items: [
--- internal/usenet/nzb/nzb.go
+++ internal/usenet/nzb/nzb.go
@@ -56,6 +56,10 @@ type File struct {
 	number int    `xml:"-"`
 }
 
+func (f *File) GetName() string {
+	return f.name
+}
+
 type NZB struct {
 	XMLName xml.Name `xml:"nzb"`
 	Head    *Head    `xml:"head"`
