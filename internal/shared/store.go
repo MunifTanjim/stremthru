ecffff02b02614dfc8f6fa7e55e836b8a37828cb
Munif Tanjim <hello@muniftanjim.dev>
2026-05-13 23:52:12 +0600

    fix(store/realdebrid): update user-agent

--- internal/shared/store.go
+++ internal/shared/store.go
@@ -57,7 +57,7 @@ var ocStore = offcloud.NewStoreClient(&offcloud.StoreClientConfig{
 })
 var rdStore = realdebrid.NewStoreClient(&realdebrid.StoreClientConfig{
 	HTTPClient: config.GetHTTPClient(config.StoreTunnel.GetTypeForAPI("realdebrid")),
-	UserAgent:  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+	UserAgent:  util.MustDecodeBase64("Y3VybC84LjcuMQ=="),
 })
 var stStore = stremthru.NewStoreClient(&stremthru.StoreClientConfig{})
 var tbStore = torbox.NewStoreClient(&torbox.StoreClientConfig{
