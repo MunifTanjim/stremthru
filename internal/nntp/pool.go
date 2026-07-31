e824f6bf2c3806eb420a8674580b7f60a04036b1
Munif Tanjim <hello@muniftanjim.dev>
2026-07-31 12:54:00 +0600

    revert(nntp): drop multi-command dead codes

--- internal/nntp/pool.go
+++ internal/nntp/pool.go
@@ -21,8 +21,6 @@ type PoolConfig struct {
 	MinSize int32
 	MaxSize int32
 
-	MaxInflightCommands uint8
-
 	HealthCheckTimeout time.Duration
 	ReconnectTimeout   time.Duration
 	ReconnectDelay     time.Duration
@@ -108,10 +106,6 @@ func (p *Pool) MaxSize() int32 {
 
 func (p *Pool) MinSize() int32 {
 	return p.config.MinSize
-}
-
-func (p *Pool) MaxInflightCommands() uint8 {
-	return p.config.MaxInflightCommands
 }
 
 func NewPool(config *PoolConfig) (*Pool, error) {
@@ -425,8 +419,4 @@ func (pc *PooledConnection) CurrentGroup() string {
 
 func (pc *PooledConnection) ProviderId() string {
 	return pc.pool.Id()
-}
-
-func (pc *PooledConnection) MaxInflightCommands() uint8 {
-	return pc.pool.MaxInflightCommands()
 }
