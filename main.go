a094a866655d3c51841b2d53b2c72121862fa4cf
Munif Tanjim <hello@muniftanjim.dev>
2026-09-15 01:37:47 +0600

    feat(debug): add admin-guarded pprof endpoints

--- main.go
+++ main.go
@@ -68,6 +68,7 @@ func main() {
 	endpoint.AddTorznabEndpoints(mux)
 	endpoint.AddNewznabEndpoints(mux)
 	endpoint.AddExperimentEndpoints(mux)
+	endpoint.AddDebugEndpoints(mux)
 	endpoint.AddEndpoints(mux)
 
 	handler := shared.RootServerContext(mux)
